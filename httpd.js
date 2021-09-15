239cf196c21dc9cc105b8f275919e49d880b5fcb
Mathieu Cantagrel <mathieu.cantagrel@efrei.net>
2021-09-15 11:02:19 +0200

    first exercise

--- httpd.js
+++ httpd.js
@@ -1,8 +1,10 @@
 var http = require("http");
 
 var server = http.createServer((req, res) => {
-  console.log(req.method + " " + req.url + " " + req.httpVersion);
-  res.send("hello word !");
+  res.setHeader("200", {
+    "Content-type": "text/html",
+  });
+  res.write("<html><body><h1>Hello World!</h1></body></html>");
   res.end();
 });
 
