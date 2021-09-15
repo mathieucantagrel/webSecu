a72bfda4c33d4d962016ae314db192cab4f9fcc7
Mathieu Cantagrel <mathieu.cantagrel@efrei.net>
2021-09-15 12:03:04 +0200

    serving dynamic content from static file

--- httpd.js
+++ httpd.js
@@ -1,21 +1,45 @@
 var http = require("http");
 var fs = require("fs");
+var url = require("url");
+var path = require("path");
 
 var server = http.createServer((req, res) => {
-  let file = __dirname + "/public/index.html";
+  let parsedUrl = url.parse(req.url, true);
+
+  let requestedPath = parsedUrl.path.replace(/^\/+|\/+$/g, "");
+
+  if (requestedPath == "") {
+    requestedPath = "index.html";
+  }
+  console.log("requested path: " + requestedPath);
+
+  let file = __dirname + "/public/" + requestedPath;
   fs.readFile(file, function (err, content) {
     if (err) {
       res.writeHead(404);
       res.end();
     }
 
-    res.setHeader("200", {
-      "Content-type": "text/html",
-    });
-    res.end(content);
-  });
+    let mimeType;
+    switch (path.extname(requestedPath)) {
+      case ".css":
+        mimeType = "text/css";
+        break;
+      case ".js":
+        mimeType = "text/javascript";
+        break;
 
-  //   res.end();
+      default:
+        mimeType = "text/html";
+        break;
+    }
+
+    console.log("mime-type: ", mimeType);
+
+    res.writeHead(200, { "Content-type": mimeType });
+    res.write(content);
+    res.end();
+  });
 });
 
 server.listen(8000);
